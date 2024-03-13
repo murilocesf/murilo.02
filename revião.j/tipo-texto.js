9d269e4022f86f3a153550b5716456bfd542752d
murilocesf <162130133+murilocesf@users.noreply.github.com>
2024-03-13 12:35:12 -0300

    Add files via upload

---
+++ revião.j/tipo-texto.js
@@ -0,0 +1,10 @@
+const nomeAluno="Beatriz";
+const maeAluno="Juliana";
+
+consol.log(nomeAluno);
+
+const idadeAluno=17;
+
+console.log(nomeAluno * idadeAluno);
+
+console.log(nomeAluno + maeAluno);//concatenação
---
+++ revião.j/tipo_numero.js
@@ -0,0 +1,16 @@
+//numeros inteiros
+const primeiroNumero = 10;
+const segundoNumero = 20;
+console.log(primeiroNumero + segundoNumero);
+
+//ponto flutuante
+const primeiroDecimal= 3.14;
+const segundoDecimal= 0.5;
+
+console.long(primeiroDecimal / segundoDecimal);
+console.long(primeiroDecimal - segundoDecimal);
+console.long(primeiroDecimal * segundoDecimal);
+
+const divisao = primeiroDecimal / segundoDecimal;
+console.log(divisao.toFixed(1));
+
